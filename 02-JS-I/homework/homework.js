06fb2e8e93d5af1a2c32d2a0abd2c0d301c65426
romerolgg <leandro.romero06@outlook.es>
2022-07-29 01:54:03 -0300

    Tarea JSI Completed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -183,7 +183,7 @@ function combinarNombres(nombre, apellido) {
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
-  // Tu código:
+  // Tu código:.
   return 'Hola ' + nombre + '!';
 }
 
